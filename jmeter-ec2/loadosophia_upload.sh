54b094d2a25f7e0bb15a2571db3c161d16c94259
dirceu <dirceu@scarface.(none)>
2014-12-17 20:18:18 -0200

    arquivo desnecessario

--- jmeter-ec2/loadosophia_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Adding csv column names"
-sed -i "1itimeStamp,elapsed,label,responseCode,responseMessage,threadName,dataType,success,bytes,grpThreads,allThreads,Latency,Hostname" $1
-
-echo "Gzipping..."
-gzip $1
